a7674866aef1ab6611472524c53df05642d1e192
SARATHKUMAR-T <123524854+SARATHKUMAR-T@users.noreply.github.com>
2023-03-28 13:59:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function foo(){
+    var nam=document.getElementById('un').value;
+console.log(nam);
+
+var email=document.getElementById('email').value;
+console.log(email);
+
+var pass=document.getElementById('pd').value;
+console.log(pass);
+
+}
+
